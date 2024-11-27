488b4c3ae30f23900cab0eb1ade8c21cace19985
Matteo Tagliapietra <matteo.tagliapietra.dev@gmail.com>
2024-11-27 13:28:52 +0100

    Refactor log printing to dynamically adjust terminal width; add support for log tags and improve caller information display

--- utils.go
+++ utils.go
@@ -3,6 +3,7 @@ package logger
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/Tagliapietra96/tui"
 	"github.com/Tagliapietra96/tui/opts"
@@ -23,13 +24,17 @@ func getTerminalSize() (int, int) {
 }
 
 func printLogs(logs []*log) {
-	w, _ := getTerminalSize()
-	w -= 4
-	page := tui.NewStyle(opts.Margin(1, 2), opts.Width(w))
+	w := 120
+	tw, _ := getTerminalSize()
+	if tw > 0 && tw < w {
+		w = tw
+	}
+	page := tui.NewStyle(opts.Margin(1, 2), opts.FitWidth(w))
+	ls := make([]string, len(logs))
 	for _, log := range logs {
-		l := tui.NewStyle(opts.Padding(1))
-		l = l.Border(lipgloss.NormalBorder(), true)
-		tui.Config(&l, opts.FitWidth(w))
+		l := tui.NewStyle(opts.Padding(0, 1))
+		l = l.Border(lipgloss.RoundedBorder(), true)
+		tui.Config(&l, opts.FitWidth(w-2))
 		color := tui.ColorMuted
 		switch log.level {
 		case Debug:
@@ -48,14 +53,19 @@ func printLogs(logs []*log) {
 
 		logTitle := tui.NewStyle(opts.Color(nil, nil, tui.ColorLightMuted)).Border(lipgloss.NormalBorder(), false, false, true, false)
 		level := tui.Render(log.level.String(), opts.Color(color))
-		timestamp := tui.Render(log.timestamp.String(), opts.Color(tui.ColorMuted), opts.Right, opts.Width(w-2-lipgloss.Width(level)))
-		caller := tui.Render(fmt.Sprintf("at %s:%d - %s", log.callerFile, log.callerLine, log.callerFunction), opts.Color(tui.ColorMuted), opts.Left, opts.Width(w-2))
-		tui.Concat(&logTitle, level, timestamp, "\n", caller)
+		timestamp := tui.Render(log.timestamp.String(), opts.Color(tui.ColorMuted), opts.Right, opts.Width(w-4-lipgloss.Width(level)))
+		caller := tui.Render(fmt.Sprintf("at %s:%d - %s", filepath.Base(log.callerFile), log.callerLine, log.callerFunction), opts.Color(tui.ColorMuted), opts.Left)
+		tags := tui.NewStyle(opts.Color(tui.ColorLightMuted), opts.Right)
+		for _, tag := range log.tags {
+			tui.Concat(&tags, fmt.Sprintf("🔖 %s", tag))
+		}
+		tui.Config(&tags, opts.Width(w-4-lipgloss.Width(caller)))
+		tui.Concat(&logTitle, level, timestamp, "\n", caller, tags.String())
 
-		message := tui.Render(log.message, opts.Left, opts.Padding(1, 0, 0, 0), opts.Width(w-2))
+		message := tui.Render(log.message, opts.Left, opts.Padding(1, 0), opts.Width(w-4))
 		tui.Concat(&l, logTitle.String(), message)
-		tui.Concat(&page, l.String())
+		ls = append(ls, l.String())
 	}
 
-	fmt.Println(page.String())
+	fmt.Print(page.Render(lipgloss.JoinVertical(lipgloss.Left, ls...)))
 }
